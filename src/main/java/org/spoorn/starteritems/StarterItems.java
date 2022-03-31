9f38fe36deded1549b8def2a5208b7eb71f97f77
spoorn <spookump@gmail.com>
2022-03-31 05:40:25 -0700

    Initial commit local

---
+++ src/main/java/org/spoorn/starteritems/StarterItems.java
@@ -0,0 +1,10 @@
+package org.spoorn.starteritems;
+
+import net.fabricmc.api.ModInitializer;
+
+public class StarterItems implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/spoorn/starteritems/client/StarterItemsClient.java
@@ -0,0 +1,14 @@
+package org.spoorn.starteritems.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Log4j2
+@Environment(EnvType.CLIENT)
+public class StarterItemsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
