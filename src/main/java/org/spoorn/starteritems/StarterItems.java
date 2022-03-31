762a9a3de71079dcab93d791903c2b19eb312d55
spoorn <spookump@gmail.com>
2022-03-31 16:27:46 -0700

    Bump to 1.1
    Add capability to set count of starter item to give to player

--- src/main/java/org/spoorn/starteritems/StarterItems.java
+++ src/main/java/org/spoorn/starteritems/StarterItems.java
@@ -10,14 +10,18 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import org.spoorn.starteritems.config.ModConfig;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Log4j2
 public class StarterItems implements ModInitializer {
     
     public static final String MODID = "starteritems";
     private static final String JOINED_ID = MODID + "_joined";
+    private static final Pattern ITEM_REGEX = Pattern.compile("^((?<count>\\d+)\\s+)?\\s*(?<item>[\\w:]+)$");
     
     @Override
     public void onInitialize() {
@@ -26,37 +30,56 @@ public class StarterItems implements ModInitializer {
         // Config
         ModConfig.init();
         
+        // Validate and cache starter items
+        List<String> starterItems = ModConfig.get().starterItems;
+        List<ItemStack> starterItemStacks = new ArrayList<>();
+        for (String starterItem : starterItems) {
+            Matcher matcher = ITEM_REGEX.matcher(starterItem.trim());
+            
+            if (!matcher.matches()) {
+                throw new RuntimeException("Starter Item {" + starterItem + "} is not in a valid format.  " +
+                        "Please check the config file at config/starteritems.json5 for acceptable formats.");
+            }
+            
+            String countStr = matcher.group("count");
+            String itemStr = matcher.group("item");
+
+            Identifier identifier = new Identifier(itemStr);
+            if (!Registry.ITEM.containsId(identifier)) {
+                throw new RuntimeException("Starter Item {" + itemStr + "} does not exist in the Item registry.  " +
+                        "Are you sure the identifier is correct?");
+            }
+            
+            Item item = Registry.ITEM.get(identifier);
+            int count = countStr == null ? 1 : Integer.parseInt(countStr);
+            
+            starterItemStacks.add(new ItemStack(item, count));
+        }
+        
         // On player log in, give starter items
-        ServerEntityEvents.ENTITY_LOAD.register(((entity, world) -> {
-            if (entity instanceof ServerPlayerEntity player) {
-                Set<String> scoreboardTags = player.getScoreboardTags();
-                if (!scoreboardTags.contains(JOINED_ID)) {
-                    List<String> starterItems = ModConfig.get().starterItems;
-                    
-                    if (!starterItems.isEmpty()) {
-                        log.info("Player={} is joining the world for the first time.  Giving them starter items...", player.getGameProfile().getId());
+        if (!starterItemStacks.isEmpty()) {
+            log.info("StarterItems={} loaded", starterItemStacks);
+            
+            ServerEntityEvents.ENTITY_LOAD.register(((entity, world) -> {
+                if (entity instanceof ServerPlayerEntity player) {
+                    Set<String> scoreboardTags = player.getScoreboardTags();
+                    if (!scoreboardTags.contains(JOINED_ID)) {
+                        log.info("Player={} is joining the world for the first time.  Giving them starter items...", player);
 
-                        for (String itemId : starterItems) {
-                            Identifier identifier = new Identifier(itemId);
+                        if (!player.addScoreboardTag(JOINED_ID)) {
+                            throw new RuntimeException("Player " + player + " scoreboard tags are full!  Might need to find a different way to track player joined worlds");
+                        }
 
-                            if (Registry.ITEM.containsId(identifier)) {
-                                Item item = Registry.ITEM.get(identifier);
-                                boolean insertedItem = player.getInventory().insertStack(new ItemStack(item));
-                                if (!insertedItem) {
-                                    log.error("[StarterItems] Player={} inventory is full!  Cannot add anymore starter items at={} and after", player.getGameProfile().getId(), itemId);
-                                    break;
-                                }
-                            } else {
-                                log.error("[StarterItems] attempted to give player={} item={}, but was not found in the registry!", player.getGameProfile().getId(), itemId);
+                        for (ItemStack itemStack : starterItemStacks) {
+                            boolean insertedItem = player.getInventory().insertStack(itemStack);
+                            if (!insertedItem) {
+                                log.error("[StarterItems] Player={} inventory is full!  Cannot add anymore starter items at={} and after", player, itemStack);
+                                break;
                             }
                         }
                     }
-                    
-                    if (!player.addScoreboardTag(JOINED_ID)) {
-                        throw new RuntimeException("Player's scoreboard tags are full!  Gonna need to find a different way to track player joined worlds");
-                    }
                 }
-            }
-        }));
+            }));
+        }
     }
 }
--- src/main/java/org/spoorn/starteritems/config/ModConfig.java
+++ src/main/java/org/spoorn/starteritems/config/ModConfig.java
@@ -13,10 +13,13 @@ public class ModConfig implements Config {
     private static ModConfig CONFIG;
     
     @Comment("List of items that should be given to a player when they first join a world.  This should be the full item Identifier.\n" +
-            "Example:\n" +
+            "The item identifier can be prefixed with the count for that item to give to the player.\n" +
+            "\nExample:\n" +
             "\t\"starterItems\": [\n" +
             "\t\t\"minecraft:diamond\",\n" +
             "\t\t\"minecraft:iron_sword\"\n" +
+            "\t\t\"5 minecraft:apple\",\n" +
+            "\t\t\"20 minecraft:bread\"\n" +
             "\t]")
     public List<String> starterItems = new ArrayList<>();
     
